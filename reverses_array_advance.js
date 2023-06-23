179f5a82820b27663105ae281b4a12306e4d2b47
Suman Naskar <naskarsuman195@gmail.com>
2023-06-23 11:26:06 +0530

    Create a function that reverses an array

---
+++ reverses_array_advance.js
@@ -0,0 +1,8 @@
+/////////////// Create a function that reverses an array ////////////
+
+function reverseArray(array) {                 // Create a function and take a parameter as array in it.             
+
+    return array.reverse();                   // Return as reverse the array parameter using reverse() method.
+}
+console.log(reverseArray([1, 2, 3, 4, 5]));  // Call the function and in that pass an array of some numbers
+                                            // as argument and print the array as reversed.
